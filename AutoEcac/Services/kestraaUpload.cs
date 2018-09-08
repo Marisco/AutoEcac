f26279088169880755066df7aeb0ef85bc0beab3
Beatriz Rinn <beatriz.rinn@kestraa.com>
2018-09-08 20:04:41 -0300

    Post dos arquivos na Kestraa e ID Shipment

--- AutoEcac/Services/kestraaUpload.cs
+++ AutoEcac/Services/kestraaUpload.cs
@@ -9,12 +9,29 @@ namespace AutoEcac
 {
     public class kestraaUpload
     {
-        // instanciar a classe para chamar o servico
-        //kestraaUpload enviarAquivo = new kestraaUpload();
-        //envio.enviarArquivosws();
+		// instanciar a classe para chamar o servico
+		//kestraaUpload enviarAquivo = new kestraaUpload();
+		//envio.enviarArquivosws();
 
-        // Definir os parâmetros para o post provavelmente numero da di e arquivo.
-        public void enviarArquivosws()
+		// Definir os parâmetros para o post. Segue abaixo.
+		//Post dos arquivos
+		//Post: https://kestraa-upload-qa.azurewebsites.net/upload/Shipments/{ :idShipment }    “idShipment é igual a nr_processo”
+		//Exemplo de Shipment Ids: 2522243 , 2522245 , 2492940 , 2507504 , 2425127 para testar os arquivos
+
+		//Headers:
+		//Authorization:KestraaUploadServiceApiKey eyJhbGciOiJIUzI1NiJ9.S2VzdHJhYVVwbG9hZFNlcnZpY2VBcGlLZXk.ybCz7EEcTl8Hjf9lx3zjQdbd1qPcJGn_LBR3z0OGyGI
+		//Content-Type:application/json
+
+		//body:
+		//fileToUpload: arquivo que será enviado
+
+		//fileType:Import Declaration - DI, Receipt of Import - CI
+		//fileReference:NUMERODI - Extrato DI ou NUMERODI - XML Acompanhamento ou NUMERODI - XML Acompanhamento ou NUMERODI - Comprovante
+		//issueDate:data de hoje
+		//userId:99999999
+
+
+		public void enviarArquivosws()
         {
             HttpWebRequest webrequest = (HttpWebRequest)WebRequest.Create("https://kestraa-upload-qa.azurewebsites.net/upload/Shipments");
             webrequest.ContentType = "application/json";
